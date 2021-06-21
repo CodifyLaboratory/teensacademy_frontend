1195b73ea5878b95834194b6d3ae5d997654d55f
chyngyz muktarbekov <chika.7710@gmail.com>
2021-06-21 12:31:18 +0600

    fixx

--- js__chika/script.js
+++ js__chika/script.js
@@ -1,5 +1,7 @@
 let burger = document.querySelector('.burger')
 let burgerContent = document.querySelector('.burger__content')
+let newsFilter = document.querySelector('.news__filter')
+let filterContent = document.querySelector('.filterContent')
 
 
 burger.addEventListener('click', function () {
